e5637fffd0fcb5ab48347e4c2fc7c652c9383d41
max <maxim.mihaluk@gmail.com>
2021-01-28 22:53:27 +0200

    Empty projects + Identity. Successfully starts.

---
+++ TinyShop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TinyShop.Areas.Identity.Data;
+using TinyShop.Data;
+
+[assembly: HostingStartup(typeof(TinyShop.Areas.Identity.IdentityHostingStartup))]
+namespace TinyShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ShopContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ShopContextConnection")));
+
+                services.AddDefaultIdentity<ShopUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ShopContext>();
+            });
+        }
+    }
+}
