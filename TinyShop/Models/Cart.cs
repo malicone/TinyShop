e2a519ea40e82e65b4b77975aed9c11a94ddeb35
max <maxim.mihaluk@gmail.com>
2024-02-21 01:28:18 +0200

    Cart implementation started.

---
+++ TinyShop/Models/Cart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TinyShop.Models
+{
+    public class Cart
+    {
+        public List<CartLine> Lines { get; set; } = new List<CartLine>();
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine? line = Lines.Where( p => p.Product.Id == product.Id ).FirstOrDefault();
+            if (line == null)
+            {
+                Lines.Add( new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                } );
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+        public void RemoveLine(Product product) =>
+        Lines.RemoveAll( l => l.Product.Id == product.Id );
+        public decimal ComputeTotalValue() => Lines.Sum( e => e.Product.Price * e.Quantity );
+
+        public void Clear() => Lines.Clear();
+    }
+    public class CartLine
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; } = new();
+        public int Quantity { get; set; }
+    }
+}
+}
